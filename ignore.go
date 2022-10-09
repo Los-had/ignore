9901b1e64da421eb94552a55536e24579b78c21d
Los-had <githubtestefinal@gmail.com>
2022-10-08 23:24:29 -0300

    Main functions created

--- ignore.go
+++ ignore.go
@@ -12,3 +12,15 @@ import (
 func main() {
 
 }
+
+func HandleFile(content string) error {
+	return
+}
+
+func GetRawFileContent(lang string) (string, error) {
+	return
+}
+
+func NormalizeUserInput(input string) string {
+	return
+}
