3f2add79e5622080151d5cff2230ae43ecb67c11
Los-had <githubtestefinal@gmail.com>
2022-10-12 19:36:28 -0300

    CLI commands created

--- ignore.go
+++ ignore.go
@@ -9,8 +9,17 @@ import (
 	"strings"
 )
 
-func main() {
+var RawGithubURL = "https://raw.githubusercontent.com/github/gitignore/main/"
 
+func main() {
+	language := flag.String("language", "", "Language of the gitignore file")
+	flag.Parse()
+
+	if *language != "" {
+		log.Println("Downloading gitignore from the ", *language, " language")
+	} else {
+		log.Fatalln("Language not specified")
+	}
 }
 
 func HandleFile(content string) error {
